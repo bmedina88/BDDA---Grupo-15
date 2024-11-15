3332e4eb665ce49d1c3fc5613cf9af3b6733a179
Braian Medina <beybraian@gmail.com>
2024-11-14 22:57:56 -0300

    Rutas dinamicas
    Entrega 4
    Se agregaron rutas dinamicas mediante los párametros para todos los procedimientos almancenados de importación de archivos.

--- Grupo_15_Entrega_4.sql
+++ Grupo_15_Entrega_4.sql
@@ -8,7 +8,7 @@
 --				Di Rocco, Sebastian Martin		DNI: 41292371
 
 use Com5600G15;
---Activar consultas distribuidas
+--Activa consultas distribuidas
 GO
 EXEC sp_configure 'Show Advanced', 1
 RECONFIGURE
@@ -29,91 +29,98 @@ GO
 
 
 
--- Importación sucursal
+-- Importación sucursal (Usa: Informacion_complementaria.xlsx)
 CREATE OR ALTER PROCEDURE Super.importarSucursal(
 @ruta nvarchar(max))
 AS
-BEGIN	
-	IF OBJECT_ID('tempdb..#SucursalTemp') IS NULL
-		CREATE TABLE #SucursalTemp(
-			ciudad nvarchar(max),
-			sucursal nvarchar(max),
-			direccion nvarchar(max),
-			horario nvarchar(max),
-			telefono nvarchar(max))
+BEGIN
+	DROP TABLE IF EXISTS #SucursalTemp;
+
+	CREATE TABLE #SucursalTemp(
+		ciudad nvarchar(max),
+		sucursal nvarchar(max),
+		direccion nvarchar(max),
+		horario nvarchar(max),
+		telefono nvarchar(max))
 
 
-	
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		INSERT INTO #SucursalTemp
+			SELECT * FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@ruta+''',
+			''SELECT * FROM [Sucursal$]'');'
 
-	INSERT INTO #SucursalTemp
-		SELECT * FROM OPENROWSET(
-		'Microsoft.ACE.OLEDB.12.0',
-		'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Informacion_complementaria.xlsx',
-		'SELECT * FROM [Sucursal$]');
-	
+	EXEC sp_executesql @sql
 
 	INSERT INTO Super.Sucursal(ciudad, sucursal, direccion, horario, telefono)
 	SELECT ciudad, sucursal, direccion, horario, telefono
 	FROM #SucursalTemp AS T
 	WHERE T.sucursal NOT IN (SELECT S.sucursal FROM Super.Sucursal AS S);
-	
-	DROP TABLE IF EXISTS #SucursalTemp;
 END
 GO
 
 
--- Importación Medios de Pago
+
+-- Importación Medios de Pago (Usa: Informacion_complementaria.xlsx)
 CREATE OR ALTER PROCEDURE Venta.ImportarMedioPago(
 @ruta nvarchar(max) )
 AS
 BEGIN
-	IF OBJECT_ID('tempdb..#MedioPagoTemp') IS NULL
-		CREATE TABLE #MedioPagoTemp(
-			descripcion nvarchar(max))
+	DROP TABLE IF EXISTS #MedioPagoTemp;
+	CREATE TABLE #MedioPagoTemp(
+		descripcion nvarchar(max))
 	
 
-	INSERT INTO #MedioPagoTemp
-		SELECT * FROM OPENROWSET(
-		'Microsoft.ACE.OLEDB.12.0',
-		'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Informacion_complementaria.xlsx',
-		'SELECT [F2] FROM [medios de pago$]');
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		INSERT INTO #MedioPagoTemp
+			SELECT * FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@ruta+''',
+			''SELECT [F2] FROM [medios de pago$]'');'
+
+	EXEC sp_executesql @sql
 	
 
 	INSERT INTO Venta.MedioPago(descripcion)
 	SELECT descripcion
 	FROM #MedioPagoTemp as T
 	WHERE T.descripcion NOT IN (SELECT M.descripcion FROM Venta.MedioPago AS M);
-
-	DROP TABLE IF EXISTS #MedioPagoTemp;
 END
 GO
 
 
---Importación Empleados
+--Importación Empleados (Usa: Informacion_complementaria.xlsx)
 CREATE OR ALTER PROCEDURE Super.ImportarEmpleados(
 	@RUTA nvarchar(max) )
 AS
 BEGIN
-		IF OBJECT_ID('tempdb..#EmpleadoTemp') IS NULL
-		CREATE TABLE #EmpleadoTemp(
-			legajo nvarchar(max),
-			nombre nvarchar(max),
-			apellido nvarchar(max),
-			dni nvarchar(max),
-			direccion nvarchar(max),
-			emailPersonal nvarchar(max),
-			emailEmpresa nvarchar(max),
-			cuil nvarchar(max),
-			cargo nvarchar(max),
-			sucursal nvarchar(max),
-			turno nvarchar(max))
+	DROP TABLE IF EXISTS #EmpleadoTemp;
+	CREATE TABLE #EmpleadoTemp(
+		legajo nvarchar(max),
+		nombre nvarchar(max),
+		apellido nvarchar(max),
+		dni nvarchar(max),
+		direccion nvarchar(max),
+		emailPersonal nvarchar(max),
+		emailEmpresa nvarchar(max),
+		cuil nvarchar(max),
+		cargo nvarchar(max),
+		sucursal nvarchar(max),
+		turno nvarchar(max))
 
 
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
 		INSERT INTO #EmpleadoTemp
 			SELECT * FROM OPENROWSET(
-			'Microsoft.ACE.OLEDB.12.0',
-			'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Informacion_complementaria.xlsx',
-			'SELECT * FROM [Empleados$]');
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@ruta+''',
+			''SELECT * FROM [Empleados$]'');'
+
+	EXEC sp_executesql @sql;
 
 		WITH EmpleadoConFk AS
 		(SELECT T.legajo, T.dni, T.cuil, T.emailPersonal, T.cargo, T.turno, (SELECT S.idSucursal FROM Super.Sucursal AS S WHERE S.sucursal=T.sucursal) AS idSucursal
@@ -123,87 +130,98 @@ BEGIN
 		SELECT * 
 		FROM EmpleadoConFk as F
 		WHERE F.legajo NOT IN (SELECT E.idEmpleado FROM Super.Empleado AS E);	
-		
-		DROP TABLE IF EXISTS #EmpleadoTemp;
 END
 GO
 
 
---Importación de Categorias
+--Importación de Categorias (Usa: Informacion_complementaria.xlsx)
 CREATE OR ALTER PROCEDURE Producto.ImportarCategorias(
 	@RUTA nvarchar(max))
 AS
 BEGIN
 	DROP TABLE IF EXISTS #CategoriaTemp;
-	IF OBJECT_ID('tempdb..#CategoriaTemp') IS NULL
-		CREATE TABLE #CategoriaTemp(
-			categoria nvarchar(max),
-			producto nvarchar(max))
+	CREATE TABLE #CategoriaTemp(
+		categoria nvarchar(max),
+		producto nvarchar(max))
 
-	INSERT INTO #CategoriaTemp
-		SELECT * FROM OPENROWSET(
-		'Microsoft.ACE.OLEDB.12.0',
-		'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Informacion_complementaria.xlsx',
-		'SELECT * FROM [Clasificacion productos$]');	
+
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		INSERT INTO #CategoriaTemp
+			SELECT * FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@ruta+''',
+			''SELECT * FROM [Clasificacion productos$]'');'
+
+	EXEC sp_executesql @sql
+
 
 	INSERT INTO Producto.Categoria (nombre)
 	SELECT DISTINCT T.categoria
 	FROM #CategoriaTemp AS T
 	WHERE T.categoria NOT IN (SELECT C.nombre FROM Producto.Categoria AS C)
-
-
-	DROP TABLE IF EXISTS #CategoriaTemp;
 END
 GO
 
---Importación de Productos Electrónicos 
+
+--Importación de Productos Electrónicos (Usa: Electronic accessories.xlsx)
 CREATE OR ALTER PROCEDURE Producto.ImportarProductosElectronicos(
 	@RUTA nvarchar(max))
 AS
 BEGIN
+	DROP TABLE IF EXISTS #ProductoElectronicoTemp;
+	CREATE TABLE #ProductoElectronicoTemp(
+		producto nvarchar(max),
+		precio nvarchar(max))
+
 	IF (NOT EXISTS (SELECT * FROM Producto.Categoria WHERE nombre='Electrónicos'))
 		INSERT INTO Producto.Categoria (nombre) VALUES ('Electrónicos')
-	
-	IF OBJECT_ID('tempdb..#ProductoElectronicoTemp') IS NULL
-		CREATE TABLE #ProductoElectronicoTemp(
-			producto nvarchar(max),
-			precio nvarchar(max))
 
-	INSERT INTO #ProductoElectronicoTemp
-		SELECT * FROM OPENROWSET(
-		'Microsoft.ACE.OLEDB.12.0',
-		'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Productos\Electronic accessories.xlsx',
-		'SELECT * FROM [Sheet1$]');	
+
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		INSERT INTO #ProductoElectronicoTemp
+			SELECT * FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@ruta+''',
+			''SELECT * FROM [Sheet1$]'');'
+
+	EXEC sp_executesql @sql
+
 
 	INSERT INTO Producto.Producto (categoria, nombre, precio)
 	SELECT (SELECT C.idCategoria FROM Producto.Categoria AS C WHERE nombre='Electrónicos'), * 
 	FROM #ProductoElectronicoTemp AS T
 	WHERE T.producto NOT IN (SELECT P.nombre FROM Producto.Producto AS P)
-
-	DROP TABLE IF EXISTS #ProductoElectronicoTemp;
 END
 GO
 
 
---Importación Productos Importados
+--Importación Productos Importados (Usa: Productos_importados.xlsx)
 CREATE OR ALTER PROCEDURE Producto.ImportarProductosImportado(
 	@RUTA nvarchar(max))
 AS
 BEGIN
-	IF OBJECT_ID('tempdb..#ProductoImportadoTemp') IS NULL
-		CREATE TABLE #ProductoImportadoTemp(
-			id nvarchar(max),
-			nombre nvarchar(max),
-			proveedor nvarchar(max),
-			categoria nvarchar(max),
-			cantidadPorUnidad nvarchar(max),
-			precioPorUnidad nvarchar(max))
+	DROP TABLE IF EXISTS #ProductoImportadoTemp;
+	CREATE TABLE #ProductoImportadoTemp(
+		id nvarchar(max),
+		nombre nvarchar(max),
+		proveedor nvarchar(max),
+		categoria nvarchar(max),
+		cantidadPorUnidad nvarchar(max),
+		precioPorUnidad nvarchar(max))
 
-	INSERT INTO #ProductoImportadoTemp
-		SELECT * FROM OPENROWSET(
-		'Microsoft.ACE.OLEDB.12.0',
-		'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Productos\Productos_importados.xlsx',
-		'SELECT * FROM [Listado de Productos$]');	
+
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		INSERT INTO #ProductoImportadoTemp
+			SELECT * FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@ruta+''',
+			''SELECT * FROM [Listado de Productos$]'');'
+
+	EXEC sp_executesql @sql
+
 
 	--Creación de categorias no registradas 
 
@@ -212,7 +230,7 @@ BEGIN
 	FROM #ProductoImportadoTemp AS T
 	WHERE T.categoria NOT IN (SELECT C.nombre FROM Producto.Categoria AS C);
 
-	---SS
+	--
 
 	WITH ProductoImportadoConFk AS 
 	( 
@@ -223,14 +241,11 @@ BEGIN
 	SELECT *
 	FROM ProductoImportadoConFk AS C
 	WHERE NOT EXISTS(SELECT * FROM Producto.Producto AS P WHERE P.nombre=C.nombre AND P.categoria=C.idCategoria) 
-	
-
-	DROP TABLE IF EXISTS #ProductoImportadoTemp;
 END
 GO
 
 
---Importación Productos Catalogo
+--Importación Productos Catalogo (Usa: catalogo.csv, Informacion_complementaria,xlsx)
 CREATE OR ALTER PROCEDURE Producto.ImportarProductosCatalogo(
 	@RUTACATALAGO nvarchar(max),
 	@RUTACATEGORIAS nvarchar(max))
@@ -238,41 +253,47 @@ AS
 BEGIN
 	DROP TABLE IF EXISTS #CategoriaTemp;
 	--Precarga de Categorias y su detalle para su posterior busqueda
-	IF OBJECT_ID('tempdb..#CategoriaTemp') IS NULL
-		CREATE TABLE #CategoriaTemp(
-			categoria nvarchar(max),
-			producto nvarchar(max))
+	CREATE TABLE #CategoriaTemp(
+		categoria nvarchar(max),
+		producto nvarchar(max));
 
-	INSERT INTO #CategoriaTemp (categoria, producto)
-		SELECT *
-		FROM OPENROWSET(
-		'Microsoft.ACE.OLEDB.12.0',
-		'Excel 12.0;Database=C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Informacion_complementaria.xlsx',
-		'SELECT * FROM [Clasificacion productos$]');
+
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		INSERT INTO #CategoriaTemp
+			SELECT * FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0;Database='+@RUTACATEGORIAS+''',
+			''SELECT * FROM [Clasificacion productos$]'');';
+
+	EXEC sp_executesql @sql;
 
 
 	--Importación catalago
 	DROP TABLE IF EXISTS #CatalogoTemp;
-	IF OBJECT_ID('tempdb..#CatalogoTemp') IS NULL
-		CREATE TABLE #CatalogoTemp(
-			id nvarchar(max),
-			categoria nvarchar(max),
-			nombre nvarchar(max),
-			precio nvarchar(max),
-			precio_referencia nvarchar(max),
-			unidad_referencia nvarchar(max),
-			fecha nvarchar(max))
+	CREATE TABLE #CatalogoTemp(
+		id nvarchar(max),
+		categoria nvarchar(max),
+		nombre nvarchar(max),
+		precio nvarchar(max),
+		precio_referencia nvarchar(max),
+		unidad_referencia nvarchar(max),
+		fecha nvarchar(max));
 
-	BULK INSERT #CatalogoTemp
-	FROM 'C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Productos\catalogo.csv'
-	WITH 
-	(	
-		FORMAT = 'CSV',
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0a',
-		CODEPAGE = '65001'
-	);
+	
+	DECLARE @sql2 nvarchar(max);
+	SELECT @sql2 = '
+		BULK INSERT #CatalogoTemp
+		FROM '''+@RUTACATALAGO+'''
+		WITH 
+		(	
+			FORMAT = ''CSV'',
+			FIRSTROW = 2,
+			FIELDTERMINATOR = '','',
+			ROWTERMINATOR = ''0x0a'',
+			CODEPAGE = ''65001''
+		);'
+	EXEC sp_executesql @sql2;
 
 
 	WITH CategoriaConFk AS
@@ -292,23 +313,16 @@ BEGIN
 	SELECT *
 	FROM CatalogoConFk AS CACFK
 	WHERE NOT EXISTS(SELECT * FROM Producto.Producto AS P WHERE P.categoria=CACFK.idCategoria AND P.nombre=CACFK.nombre)
-
-
-
-
-		DROP TABLE IF EXISTS #CategoriaTemp;
-		DROP TABLE IF EXISTS #CatalogoTemp;
 END
 GO
 
 
---Importación Ventas
+--Importación Ventas (Usa: Ventas_registradas.csv)
 CREATE OR ALTER PROCEDURE Venta.ImportarVentas(
 	@RUTA NVARCHAR(MAX))
 AS
 BEGIN
 	DROP TABLE IF EXISTS #VentaTemp
-
 	CREATE TABLE #VentaTemp(
 	idFactura nvarchar(max),
 	tipoFactura nvarchar(max),
@@ -324,17 +338,20 @@ BEGIN
 	empleado nvarchar(max),
 	idPago nvarchar(max))
 
+	DECLARE @sql nvarchar(max);
+	SELECT @sql = '
+		BULK INSERT #VentaTemp
+		FROM '''+@RUTA+'''
+		WITH 
+		(	
+			FORMAT = ''CSV'',
+			FIRSTROW = 2,
+			FIELDTERMINATOR = '';'',
+			ROWTERMINATOR = ''0x0a'',
+			CODEPAGE = ''65001''
+		);'
+	EXEC sp_executesql @sql;
 
-	BULK INSERT #VentaTemp
-	FROM 'C:\Users\beybr\OneDrive\Escritorio\TP_integrador_Archivos\Ventas_registradas.csv'
-	WITH 
-	(	
-		FORMAT = 'CSV',
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ';',
-		ROWTERMINATOR = '0x0a',
-		CODEPAGE = '65001'
-	);
 
 --Update para acentos
 	UPDATE #VentaTemp
@@ -389,15 +406,11 @@ GROUP BY T.idFactura,T.tipoFactura, idPago, fecha, hora, empleado, medioPago, ti
 --Import VentaDetalle
 INSERT INTO Venta.VentaDetalle(cantidad, idfactura, producto)
 SELECT T.cantidad, (SELECT F.id FROM Venta.Factura AS F WHERE F.idfactura=T.idFactura),
-		(SELECT P.idProducto FROM Producto.Producto AS P WHERE P.nombre = T.producto AND P.precio = T.precioUnitario)
+		(SELECT TOP(1) P.idProducto FROM Producto.Producto AS P WHERE P.nombre = T.producto AND P.precio = T.precioUnitario)
 FROM #VentaTemp AS T
 
 --NOTA: PENSAR UN UPDATE AL INGRESAR PRODUCTO REPETIDO 
-
 END
 GO
-
-
-
 
 
